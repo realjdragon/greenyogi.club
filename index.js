6f6ef20543538aa94e5bfa50bbd5f3dd6ba69687
Jaeyong Sin <jdragon@JaeyongBook.local>
2020-06-23 16:04:47 +0900

    왼쪽 페이지 dimmed 처리

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 (function() {
   document.querySelector('.project-item').addEventListener('click', (e) => {
     document.querySelector('aside').classList.toggle('on');
+    document.querySelector('.dimmed').classList.toggle('on');
   });
   
   document.querySelector('aside .btn-close').addEventListener('click', (e) => {
     document.querySelector('aside').classList.toggle('on');
+    document.querySelector('.dimmed').classList.toggle('on');
+    location.hash = '';
+  });
+
+  document.querySelector('.dimmed').addEventListener('click', (e) => {
+    document.querySelector('aside').classList.toggle('on');
+    document.querySelector('.dimmed').classList.toggle('on');
     location.hash = '';
   });
 })();
