18ff393820e26a2bb6c1fea995a78723616f8024
Jaeyong Sin <jdragon@JaeyongBook.local>
2020-06-23 14:58:51 +0900

    title typo margin-left

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector('.project-item').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('만보');
+
+  // const ui = document.querySelector('aside');
+  //  if (ui.style.display === 'none') {
+  //     ui.style.display = 'block';
+  //  } else {
+  //    ui.style.display = 'none';
+  //  }
+});
