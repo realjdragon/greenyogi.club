0a7e1c7e5b9e0dc0b693ddfd63b9c462bf88f282
Jaeyong Sin <jdragon@JaeyongBook.local>
2020-06-23 15:38:33 +0900

    즉시실행함수로 변경

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-document.querySelector('.project-item').addEventListener('click', (e) => {
-  document.querySelector('aside').classList.toggle('on');
-});
-
-document.querySelector('aside .btn-close').addEventListener('click', (e) => {
-  document.querySelector('aside').classList.toggle('on');
-  location.hash = '';
-});
+(function() {
+  document.querySelector('.project-item').addEventListener('click', (e) => {
+    document.querySelector('aside').classList.toggle('on');
+  });
+  
+  document.querySelector('aside .btn-close').addEventListener('click', (e) => {
+    document.querySelector('aside').classList.toggle('on');
+    location.hash = '';
+  });
+})();
