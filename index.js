c1cf769cd38c469e4ee98ba2f993c18fa6824bb9
Jaeyong Sin <jdragon@JaeyongBook.local>
2020-06-24 14:57:28 +0900

    main-wrapper 작성

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 (function() {
+  // 프로젝트 창 on
   document.querySelector('.project-item').addEventListener('click', (e) => {
     document.querySelector('aside').classList.toggle('on');
-    document.querySelector('.dimmed').classList.toggle('on');
+    document.querySelector('main').classList.toggle('left-half');
+    document.querySelector('.main-wrapper').classList.toggle('main-wrapper-left');
   });
   
-  document.querySelector('aside .btn-close').addEventListener('click', (e) => {
+  // 프로젝트 창 off
+  document.querySelector('aside .btn-back').addEventListener('click', (e) => {
     document.querySelector('aside').classList.toggle('on');
-    document.querySelector('.dimmed').classList.toggle('on');
+    document.querySelector('main').classList.toggle('left-half');
+    document.querySelector('.main-wrapper').classList.toggle('main-wrapper-left');
     location.hash = '';
   });
 
-  document.querySelector('.dimmed').addEventListener('click', (e) => {
-    document.querySelector('aside').classList.toggle('on');
-    document.querySelector('.dimmed').classList.toggle('on');
-    location.hash = '';
+  document.querySelector('main').addEventListener('scroll', (e) => {
+    console.log(e);
   });
 })();
